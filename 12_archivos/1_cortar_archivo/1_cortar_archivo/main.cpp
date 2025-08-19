6529f3c5bf8b4e8926cddd6a2737e82a3d365c03
antelero <lmbrambilla@gmail.com>
2025-08-19 11:42:02 -0300

    arregla ejercicio corttar archivo

--- 12_archivos/1_cortar_archivo/1_cortar_archivo/main.cpp
+++ 12_archivos/1_cortar_archivo/1_cortar_archivo/main.cpp
@@ -31,6 +31,49 @@ void cortar(char* nombre, int cant){
                 if ((currentLine % n) == 0) {
                     if (nroArchivo > 0)
                         destino.close();
+                    std::string sstm = nombre;
+                    sstm +=std::to_string(nroArchivo);
+
+
+                    // Mostrar el resultado
+                    std::cout << sstm <<  std::endl;
+
+                    destino.open(sstm.c_str());
+                    nroArchivo++;
+                }
+                destino << line << endl;
+                currentLine++;
+            }
+            if (nroArchivo > 0)
+                destino.close();
+            cout << "Se grabaron " << nroArchivo  << " archivos" << endl;
+        } else {
+            cout << "No se pudo leer el archivo" << endl;
+        }
+    } else {
+        cout << "Error: el archivo tiene " << count
+             << " lineas, no se puede dividir en " << cant << " partes." << endl;
+    }
+}
+
+/*
+void cortar(char* nombre, int cant){
+    ifstream origen;
+    int count = contar(nombre);
+    cout << "Lineas: " << count << endl;
+    if (count >= cant) {
+        int n = count / cant;
+        cout << "Lineas por archivo: " << n << endl;
+        origen.open(nombre);
+        if(origen.is_open()){
+            int nroArchivo = 0;
+            ofstream destino;
+            string line;
+            int currentLine = 0;
+            while (getline(origen, line)){
+                if ((currentLine % n) == 0) {
+                    if (nroArchivo > 0)
+                        destino.close();
                     ostringstream sstm;
                     sstm << nombre << nroArchivo;
                     string newName = sstm.str();
@@ -52,7 +95,7 @@ void cortar(char* nombre, int cant){
              << " lineas, no se puede dividir en " << cant << " partes." << endl;
     }
 }
-
+*/
 int contar(char* nombre){
     ifstream origen;
     origen.open(nombre);
